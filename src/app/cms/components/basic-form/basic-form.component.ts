7192b77ce26e0e1f97be73a906e44a51da2e0154
Kevin <kevinzciro@gmail.com>
2023-01-20 09:44:26 -0500

    Formularios basicos

--- src/app/cms/components/basic-form/basic-form.component.ts
+++ src/app/cms/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,28 +8,112 @@ import { FormControl } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl("");
-  emailField = new FormControl("");
-  numberField = new FormControl("");
-  phoneField = new FormControl("");
-  colorField = new FormControl('#000000');
-  dateField = new FormControl("");
+  constructor(
+    private formBuilder:FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+      name: ["", [Validators.required, Validators.maxLength(15), Validators.pattern(/^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}\s?){2,4}$/)]],
+      email: ["", [Validators.required, Validators.email]],
+      number: ["", [Validators.required, Validators.min(18), Validators.max(99)]],
+      phone: ["", Validators.required],
+      color: ["#00000"],
+      date: [""],
+      category: ["ctegory-3"],
+      tag: [""],
+      agree: [false, [Validators.requiredTrue]],
+      gender: [""],
+      zone: [""],
+    });
+  }
 
-  categoryField = new FormControl("category-3");
-  tagField = new FormControl("");
+  private form: FormGroup;
 
-  agreeField = new FormControl(false);
+  // nameField = new FormControl("", [Validators.required, Validators.maxLength(15)]);
+  // emailField = new FormControl("");
+  // numberField = new FormControl("");
+  // phoneField = new FormControl("");
+  // colorField = new FormControl('#000000');
+  // dateField = new FormControl("");
+
+  // categoryField = new FormControl("category-3");
+  // tagField = new FormControl("");
+
+  // agreeField = new FormControl(false);
+  // genderField = new FormControl("");
+  // zoneField = new FormControl("");
 
   ngOnInit(): void {
-    this.nameField.valueChanges
+    this.nameField?.valueChanges
     .subscribe(
       value => {
         console.log(value);
-      }
-    )
-  }
-  getNameValue(){
-    console.log(this.nameField.value);
+    });
+
+    this.form.valueChanges
+      .subscribe(
+        value => {
+          console.log(value);
+    });
 
   }
+
+    getNameValue(){
+      console.log(this.nameField?.value);
+    }
+
+    get isNameFieldValid(){
+      return this.nameField?.touched && this.nameField?.valid;
+    }
+
+    get isNameFieldInvalid(){
+      return this.nameField?.touched && this.nameField?.invalid;
+    }
+
+  get nameField(){
+    return this.form.get('name');
+  }
+  get emailField(){
+    return this.form.get('email');
+  }
+  get numberField(){
+    return this.form.get('number');
+  }
+  get phoneField(){
+    return this.form.get('phone');
+  }
+  get colorField(){
+    return this.form.get('color');
+  }
+  get dateField(){
+    return this.form.get('date');
+  }
+  get categoryField(){
+    return this.form.get('category');
+  }
+  get tagField(){
+    return this.form.get('tag');
+  }
+  get agreeField(){
+    return this.form.get('agree');
+  }
+  get genderField(){
+    return this.form.get('gender');
+  }
+  get zoneField(){
+    return this.form.get('zone');
+  }
+
+  get getForm(){
+    return this.form;
+  }
+
+  save(){
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+    else{
+      this.form.markAllAsTouched();
+    }
+  }
+
 }
